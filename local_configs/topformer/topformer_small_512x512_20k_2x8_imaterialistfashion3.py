e609888d33bed682388c8d900397428ecf7a61b7
LukasHedegaard <lukasxhedegaard@gmail.com>
2022-11-25 07:30:58 +0000

    Add configs for imaterialist fashion 3

---
+++ local_configs/topformer/topformer_small_512x512_20k_2x8_imaterialistfashion3.py
@@ -0,0 +1,43 @@
+_base_ = [
+    "../_base_/datasets/imaterialist_fashion3.py",
+    "../_base_/default_runtime.py",
+    "../_base_/schedules/schedule_20k.py",
+    "./topformer_small_3classes.py",
+]
+
+num_gpus = 1
+samples_per_gpu = 1
+
+# By default, models are trained on 8 GPUs with 2 images per GPU.
+# Use linear scaling rule to compensate in case of deviations
+lr = 0.00012 * num_gpus * samples_per_gpu / (2 * 8)
+
+optimizer = dict(
+    _delete_=True,
+    type="AdamW",
+    lr=lr,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            "absolute_pos_embed": dict(decay_mult=0.0),
+            "head": dict(lr_mult=10.0),
+            "norm": dict(decay_mult=0.0),
+        }
+    ),
+)
+
+lr_config = dict(
+    _delete_=True,
+    policy="poly",
+    warmup="linear",
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False,
+)
+
+
+data = dict(samples_per_gpu=samples_per_gpu)
+find_unused_parameters = True
